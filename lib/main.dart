0fa6532a982618115962ee8e381574e65beccde4
dontBlamestorming <lklone2005@gmail.com>
2022-01-14 15:49:15 +0900

    feat: Add SubBanner && Footer Sections
    SubBanner is similar with MainBanner except use Stack widget. But the image isn't fit to Container
    widget. And at top of this application, debugShowCheckedModeBanner of  MaterialApp widget property is false.

--- lib/main.dart
+++ lib/main.dart
@@ -11,6 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
@@ -291,9 +292,50 @@ class _MyHomePageState extends State<MyHomePage> {
             // Sub Banner
             Container(
               height: 200.0,
-              color: Colors.amber[200],
-              child: const Center(
-                child: Text("Sub Banner"),
+              color: Colors.amber[600],
+              child: Swiper(
+                itemBuilder: (BuildContext context, int index) {
+                  return Stack(
+                    children: [
+                      Image.network(
+                        "https://static2.selec7.com/img/seller_site_thum_img/main-slide4.png",
+                        fit: BoxFit.fill,
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[
+                          Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: const <Widget>[
+                              Text(
+                                '내 쇼핑 도와줄',
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 20.0,
+                                ),
+                              ),
+                              Text(
+                                "#취향 필요하다면?",
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 20.0,
+                                ),
+                              ),
+                              Text(
+                                "#셀렉트 하세요",
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 20.0,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ],
+                  );
+                },
+                itemCount: 3,
               ),
             ),
 
@@ -305,6 +347,62 @@ class _MyHomePageState extends State<MyHomePage> {
                 child: Text("Markets"),
               ),
             ),
+
+            // footer(consider bottom property or body property)
+            Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: Container(
+                alignment: Alignment.centerLeft,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    const Text(
+                      "(주)텐스 | info@tenthinc.com",
+                      style: TextStyle(
+                        fontSize: 11.0,
+                      ),
+                    ),
+                    const Text(
+                      "대표: 김철형 | 사업자등록번호: 828-88-00992",
+                      style: TextStyle(
+                        fontSize: 11.0,
+                      ),
+                    ),
+                    const Text(
+                      "통신판매업신고: 2019-서울강남-03168호",
+                      style: TextStyle(
+                        fontSize: 11.0,
+                      ),
+                    ),
+                    const Text(
+                      "주소: 서울시 강남구 테헤란로 79길 25-1 3층(삼성동)",
+                      style: TextStyle(
+                        fontSize: 11.0,
+                      ),
+                    ),
+                    const Text(
+                      "전화: 02-563-1271 | 팩스: 02-564-1271",
+                      style: TextStyle(
+                        fontSize: 11.0,
+                      ),
+                    ),
+                    const Padding(
+                      padding: EdgeInsets.only(top: 15.0, bottom: 10.0),
+                      child: Text(
+                        "©Tenth Inc. All Rights Reserved.",
+                        style: TextStyle(
+                          fontSize: 11.0,
+                        ),
+                      ),
+                    ),
+                    Image.network(
+                      "https://static2.selec7.com/img/seller_site_thum_img/logo.png",
+                      width: 60.0,
+                    )
+                  ],
+                ),
+              ),
+            )
           ],
         ));
   }
