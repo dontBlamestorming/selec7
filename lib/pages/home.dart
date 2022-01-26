67eb2b299b4bfb1a6c219e18b56b3555edfd910a
dontBlamestorming <lklone2005@gmail.com>
2022-01-26 14:26:51 +0900

    Feat: Add tab controller
    Tab menu is consist of main, serach, clipboard. Each of tab has viewport that consist of
    widgets. Before this commit, This menus is static.

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -24,186 +24,245 @@ class _HomeState extends State<Home> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.white,
-        iconTheme: const IconThemeData(color: Color.fromRGBO(88, 88, 238, 1.0)),
-        title: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Row(
-              children: <Widget>[
-                Image.network(
-                  "https://static2.selec7.com/img/seller_site_thum_img/logo.png",
-                  width: 120.0,
-                ),
-                const Padding(
-                  padding: EdgeInsets.only(top: 10.0),
-                  child: Text(
-                    '셀렉트, 내 취향을 팝니다.',
-                    style: TextStyle(
-                      fontSize: 10.0,
-                      color: Colors.black,
+    return DefaultTabController(
+      length: 3,
+      child: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.white,
+          iconTheme:
+              const IconThemeData(color: Color.fromRGBO(88, 88, 238, 1.0)),
+          title: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Row(
+                children: <Widget>[
+                  Image.network(
+                    "https://static2.selec7.com/img/seller_site_thum_img/logo.png",
+                    width: 120.0,
+                  ),
+                  const Padding(
+                    padding: EdgeInsets.only(top: 10.0),
+                    child: Text(
+                      '셀렉트, 내 취향을 팝니다.',
+                      style: TextStyle(
+                        fontSize: 10.0,
+                        color: Colors.black,
+                      ),
                     ),
                   ),
-                ),
-              ],
-            ),
-          ],
+                ],
+              ),
+            ],
+          ),
         ),
-      ),
-      endDrawer: Drawer(
-        child: ListView(
-          children: [
-            ListTile(
-              title: const Text("로그인을 해주세요!"),
-              onTap: () {
-                Navigator.pop(context);
-              },
-            ),
-            const Divider(
-              height: 1,
-              thickness: 1,
-            ),
-            ListTile(
-              title: const Text("공지사항 및 이용안내"),
-              onTap: () {
-                Navigator.pop(context);
-              },
-            ),
-            const Divider(
-              height: 1,
-              thickness: 1,
-            ),
-            ListTile(
-              title: const Text("알림"),
-              onTap: () {
-                Navigator.pop(context);
-              },
-            ),
-            ListTile(
-              title: const Text("장바구니 내역"),
-              onTap: () {
-                Navigator.pop(context);
-              },
-            ),
-            ListTile(
-              title: const Text("주문·배송 내역"),
-              onTap: () {
-                Navigator.pop(context);
-              },
-            ),
-            const Divider(
-              height: 1,
-              thickness: 1,
-            ),
-            const ListTile(
-              title: Text("추천상품"),
-            ),
-            Stack(
-              children: [
-                Image.network(
-                  "https://picsum.photos/250/150",
-                  fit: BoxFit.fill,
-                ),
-                Container(
-                  height: 140.0,
-                  alignment: Alignment.bottomLeft,
-                  padding: const EdgeInsets.only(left: 10.0),
-                  child: const Text(
-                    "샤베트니트🌈",
-                    style: TextStyle(
-                      fontSize: 14.0,
-                      color: Colors.white,
-                      fontWeight: FontWeight.bold,
+        endDrawer: Drawer(
+          child: ListView(
+            children: [
+              ListTile(
+                title: const Text("로그인을 해주세요!"),
+                onTap: () {
+                  Navigator.pop(context);
+                },
+              ),
+              const Divider(
+                height: 1,
+                thickness: 1,
+              ),
+              ListTile(
+                title: const Text("공지사항 및 이용안내"),
+                onTap: () {
+                  Navigator.pop(context);
+                },
+              ),
+              const Divider(
+                height: 1,
+                thickness: 1,
+              ),
+              ListTile(
+                title: const Text("알림"),
+                onTap: () {
+                  Navigator.pop(context);
+                },
+              ),
+              ListTile(
+                title: const Text("장바구니 내역"),
+                onTap: () {
+                  Navigator.pop(context);
+                },
+              ),
+              ListTile(
+                title: const Text("주문·배송 내역"),
+                onTap: () {
+                  Navigator.pop(context);
+                },
+              ),
+              const Divider(
+                height: 1,
+                thickness: 1,
+              ),
+              const ListTile(
+                title: Text("추천상품"),
+              ),
+              Stack(
+                children: [
+                  Image.network(
+                    "https://picsum.photos/250/150",
+                    fit: BoxFit.fill,
+                  ),
+                  Container(
+                    height: 140.0,
+                    alignment: Alignment.bottomLeft,
+                    padding: const EdgeInsets.only(left: 10.0),
+                    child: const Text(
+                      "샤베트니트🌈",
+                      style: TextStyle(
+                        fontSize: 14.0,
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                      ),
                     ),
                   ),
+                ],
+              ),
+              const Divider(
+                height: 1,
+                thickness: 1,
+              ),
+              ListTile(
+                title: const Text("내 웹사이트 관리"),
+                onTap: () {
+                  Navigator.pop(context);
+                },
+              ),
+              const Divider(
+                height: 1,
+                thickness: 1,
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 20.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: const <Widget>[
+                    Icon(Icons.ac_unit_rounded),
+                    Text("고객센터"),
+                    SizedBox(
+                      width: 15.0,
+                    ),
+                    Icon(Icons.ac_unit_rounded),
+                    Text("로그인"),
+                  ],
                 ),
-              ],
-            ),
-            const Divider(
-              height: 1,
-              thickness: 1,
-            ),
-            ListTile(
-              title: const Text("내 웹사이트 관리"),
-              onTap: () {
-                Navigator.pop(context);
-              },
-            ),
-            const Divider(
-              height: 1,
-              thickness: 1,
-            ),
-            Padding(
-              padding: const EdgeInsets.only(top: 20.0),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: const <Widget>[
-                  Icon(Icons.ac_unit_rounded),
-                  Text("고객센터"),
-                  SizedBox(
-                    width: 15.0,
+              ),
+            ],
+          ),
+        ),
+        backgroundColor: Colors.grey[200],
+        body: ListView(
+          children: <Widget>[
+            const MainBanner(),
+
+            Container(
+              height: 50.0,
+              child: const TabBar(
+                indicatorColor: Colors.black,
+                unselectedLabelStyle: TextStyle(
+                  fontSize: 16.0,
+                ),
+                labelStyle: TextStyle(
+                  fontWeight: FontWeight.bold,
+                ),
+                tabs: [
+                  Text(
+                    "메인",
+                    style: TextStyle(color: Colors.black),
                   ),
-                  Icon(Icons.ac_unit_rounded),
-                  Text("로그인"),
+                  Text(
+                    "상품검색",
+                    style: TextStyle(color: Colors.black),
+                  ),
+                  Text(
+                    "즐겨찾기",
+                    style: TextStyle(color: Colors.black),
+                  ),
+                ],
+              ),
+            ),
+            // const MenuTaps(),
+
+            Container(
+              height: 1074,
+              child: TabBarView(
+                children: [
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Container(
+                        height: 55.0,
+                        padding: const EdgeInsets.only(
+                          left: 20.0,
+                          top: 15.0,
+                        ),
+                        decoration: BoxDecoration(
+                          border: Border(
+                            top: BorderSide(
+                              color: Colors.grey[300]!,
+                              width: 1.0,
+                            ),
+                          ),
+                        ),
+                        child: const Text(
+                          "방금 이거 샀어요!",
+                          style: TextStyle(
+                            fontSize: 16.0,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
+                      const BuyProduct(),
+                      // Popular Product(title)
+                      Container(
+                        height: 55.0,
+                        padding: const EdgeInsets.only(
+                          left: 20.0,
+                          top: 15.0,
+                        ),
+                        child: const Text(
+                          "요즘은 이게 인기",
+                          style: TextStyle(
+                            fontSize: 16.0,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
+                      const PopularProduct(),
+
+                      ElevatedButton(
+                        child: const Icon(Icons.arrow_circle_down),
+                        onPressed: () {},
+                        style: ElevatedButton.styleFrom(
+                          shape: const CircleBorder(),
+                          padding: const EdgeInsets.all(5),
+                        ),
+                      ),
+
+                      const SubBanner(),
+                      const MarketInfo(),
+                      const Footer(),
+                    ],
+                  ),
+                  // Icon(Icons.directions_car),
+                  const Icon(Icons.directions_transit),
+                  const Icon(Icons.directions_bike),
                 ],
               ),
             ),
           ],
         ),
-      ),
-      backgroundColor: Colors.grey[200],
-      body: ListView(
-        children: <Widget>[
-          const MainBanner(),
-          const MenuTaps(),
-          Container(
-            height: 50.0,
-            padding: const EdgeInsets.only(
-              left: 15.0,
-              top: 15.0,
-              bottom: 15.0,
-            ),
-            decoration: BoxDecoration(
-              border: Border(
-                top: BorderSide(
-                  color: Colors.grey[300]!,
-                  width: 1.0,
-                ),
-              ),
-            ),
-            child: const Text("방금 이거 샀어요!"),
-          ),
-          const BuyProduct(),
-          // Popular Product(title)
-          Container(
-            height: 50.0,
-            padding: const EdgeInsets.only(
-              left: 15.0,
-              top: 15.0,
-              bottom: 15.0,
-            ),
-            child: const Text("요즘은 이게 인기"),
-          ),
-          const PopularProduct(),
-          ElevatedButton(
-            child: const Icon(Icons.arrow_circle_down),
-            onPressed: () {},
-            style: ElevatedButton.styleFrom(
-              shape: const CircleBorder(),
-              padding: const EdgeInsets.all(5),
-            ),
-          ),
-          const SubBanner(),
-          const MarketInfo(),
-          const Footer(),
-        ],
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {},
-        child: const Text("Login"),
-        backgroundColor: const Color.fromRGBO(88, 88, 238, 1.0),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {},
+          child: const Text("Login"),
+          backgroundColor: const Color.fromRGBO(88, 88, 238, 1.0),
+        ),
       ),
     );
   }
