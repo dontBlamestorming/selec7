6c7cb3d5fcead4343c7f11a9df908f95bf7c65d5
dontBlamestorming <lklone2005@gmail.com>
2022-01-27 15:30:52 +0900

    Feat: Add buy_product widget with recentSoldItems data
    this logic is similar with main_banner widget.

--- lib/widgets/buy_product.dart
+++ lib/widgets/buy_product.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 import 'package:card_swiper/card_swiper.dart';
+
+import 'package:selec7/controller/index.dart';
+import 'package:selec7/models/index.dart';
 
 class BuyProduct extends StatefulWidget {
   const BuyProduct({Key? key}) : super(key: key);
@@ -10,46 +14,112 @@ class BuyProduct extends StatefulWidget {
 }
 
 class _BuyProductState extends State<BuyProduct> {
+  AppController controller = Get.find();
+
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: 110.0,
-      child: Swiper(
-        itemBuilder: (context, index) {
-          return Container(
-            child: Card(
-              elevation: 2.0,
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(8.0),
-              ),
-              child: Container(
-                width: 300.0,
-                // height: 80.0,
-                padding: EdgeInsets.all(10.0),
-                child: Row(
-                  children: [
-                    Image.network(
-                      "https://picsum.photos/250/150",
-                      fit: BoxFit.fill,
+      height: 120.0,
+      child: FutureBuilder<Store>(
+        future: controller.store,
+        builder: (_, snapshot) {
+          if (snapshot.hasError) {
+            return Text("${snapshot.error}");
+          }
+
+          if (snapshot.hasData) {
+            return Swiper(
+              itemBuilder: (context, index) {
+                String imageLink =
+                    "https://img.selec7.com/${snapshot.data?.recentSoldItems?[index].siteGalleryRoot}/${snapshot.data?.recentSoldItems?[index].productImgInfo}";
+
+                return Container(
+                  child: Card(
+                    elevation: 2.0,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(8.0),
                     ),
-                    Column(
-                      children: const <Widget>[
-                        Text('Name'),
-                        Text('Description'),
-                        Text('Price'),
-                        Text('Recod'),
-                      ],
+                    child: Container(
+                      padding: const EdgeInsets.all(16.0),
+                      child: Row(
+                        children: <Widget>[
+                          SizedBox(
+                            height: 80.0,
+                            width: 80.0,
+                            child: ClipRRect(
+                              borderRadius: BorderRadius.circular(10),
+                              child: Image.network(
+                                imageLink,
+                                fit: BoxFit.fill,
+                              ),
+                            ),
+                          ),
+                          const SizedBox(
+                            width: 15.0,
+                          ),
+                          Container(
+                            height: 80.0,
+                            width: 200.0,
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: <Widget>[
+                                Text(
+                                  "${snapshot.data?.recentSoldItems?[index].siteName}",
+                                  style: const TextStyle(
+                                    fontSize: 12.0,
+                                  ),
+                                  overflow: TextOverflow.ellipsis,
+                                ),
+                                Text(
+                                  "${snapshot.data?.recentSoldItems?[index].title}",
+                                  style: const TextStyle(
+                                      fontSize: 14.0,
+                                      fontWeight: FontWeight.bold),
+                                  overflow: TextOverflow.ellipsis,
+                                ),
+                                Text(
+                                  "${snapshot.data?.recentSoldItems?[index].price}",
+                                  style: const TextStyle(
+                                    fontSize: 12.0,
+                                  ),
+                                  overflow: TextOverflow.ellipsis,
+                                ),
+                                Text(
+                                  "${snapshot.data?.recentSoldItems?[index].message}",
+                                  style: const TextStyle(
+                                    fontSize: 12.0,
+                                  ),
+                                  overflow: TextOverflow.ellipsis,
+                                ),
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
                     ),
-                  ],
-                ),
+                  ),
+                );
+              },
+              itemCount: snapshot.data!.recentSoldItems!.length,
+              viewportFraction: 0.9,
+              scale: 1,
+              loop: false,
+            );
+          }
+
+          return Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: const <Widget>[
+                  CircularProgressIndicator(),
+                ],
               ),
-            ),
+            ],
           );
         },
-        itemCount: 3,
-        viewportFraction: 0.9,
-        scale: 1,
-        loop: false,
       ),
     );
   }
